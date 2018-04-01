57950421ac3aa0b093db0a471194cb282f701b91
lf <mengxiangbenchi831@163.com>
2018-04-01 17:15:14 +0800

    用webpack搭建项目框架完成，uglifyjs报错待解决

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,13 +1,15 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
     entry:  __dirname + "/app/main.js",
     output: {
         path: __dirname + "/build",
-        filename: "bundle.js"
+        filename: "bundle-[hash].js"
     },
-    devtool: 'eval-source-map',
+    devtool: 'none',
     devServer: {
         contentBase: "./build",//本地服务器所加载的页面所在的目录
         historyApiFallback: true,//不跳转
@@ -46,6 +48,13 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
         }),
-        new webpack.HotModuleReplacementPlugin(),//热加载插件
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextPlugin("style.css"),
+        new CleanWebpackPlugin('build/*.*', {
+            root: __dirname,
+            verbose: true,
+            dry: false
+        })
     ],
 };
